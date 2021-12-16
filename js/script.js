2eaa4ff80e6804b25e57abb3edc1848ff417383c
Rajkumar-stackcoder <meet.r.graphicat10@gmail.com>
2021-12-16 18:00:04 +0530

    li added with effect

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     activate_image(1);
     scrollFunction();
 
@@ -24,7 +24,7 @@ function activate_image(id) {
 // this is on work so DNT 
 
 function scrollFunction() {
-    $(document).on('scroll', function() {
+    $(document).on('scroll', function () {
         if ($(window).scrollTop() > 50) {
             $('.main-nav-sec').addClass('scroll-nav');
         } else if ($(window).scrollTop() <= 50) {
@@ -32,3 +32,40 @@ function scrollFunction() {
         }
     })
 }
+
+// $('.main-li-hover').hover(function () {
+
+//     $('#sublink-of-main4').toggle('.nav-ul-hide');
+
+
+// });
+
+
+//  $li.toggle('.nav-ul-hide');
+// var link=document.getElementsByClassName('main-li-hover');
+// console.log(link);
+// for (let l = 0; l < link.length; l++) {
+//         // link[l].hover(function () {
+//         //     $('#sublink-of-main'+l).toggle('.nav-ul-hide');
+//         //  }); 
+//          link[l].onmouseover=function (e) { 
+//              $('#sublink-of-main'+l).toggle('.nav-ul-hide');
+//           }
+         
+// }
+
+// $(function () {
+//     $('.nav-ul-hover > .main-li-hover, .sub-nav > li').hover(
+//         function () {  
+//             var submenu = $(this).find('ul.sub-nav');
+//             if (submenu.hasClass('sub-nav')) {
+//                 submenu.removeClass('nav-ul-hide');
+//             }
+//         },
+//         function () {
+//             var submenu = $(this).find('ul.sub-nav');
+//             if (submenu.hasClass('sub-nav')) {
+//                 submenu.addClass('nav-ul-hide');
+//             }
+//         });
+// });
